73ab117dbd3c87584010e3ac7fd6f2c98fd49bff
Wang, Gaix <gaix.wang@intel.com>
2021-08-30 18:16:51 +0800

    [build.sh] dpkg: use lowercase package name
    Build Error:
    dpkg-source: error: source package name 'linux-lts-5.10.59-210826T212308Z'
    is illegal: character 'T' not allowed
    
    Signed-off-by: Wang, Gaix <gaix.wang@intel.com>

--- build.sh
+++ build.sh
@@ -79,7 +79,7 @@ cp $CUR_DIR/overlay.config $KSRC_DIR/.config
 make olddefconfig
 
 echo "Building the .deb package"
-nice make -j`nproc` bindeb-pkg  LOCALVERSION= KDEB_PKGVERSION=$build_id KERNELRELEASE=${kernel_p}-`make kernelversion`-${timestamp}
+nice make -j`nproc` bindeb-pkg  LOCALVERSION= KDEB_PKGVERSION=$build_id KERNELRELEASE=${kernel_p}-`make kernelversion`-${timestamp,,}
 cp .config $CUR_DIR/kernel.config
 popd
 
