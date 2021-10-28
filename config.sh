4e14a502699956241eb00aee4882421c8aeeeadb
Baolizha <baoli.zhang@intel.com>
2021-10-28 15:09:12 +0800

    Optimize the Ubuntu overlay structure (#17)
    Signed-off-by: Baoli.Zhang <baoli.zhang@intel.com>

---
+++ config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+# Global configurations which are used to build kernel overlay
+
+KVERSION=5
+KPATCHLEVEL=10
+KSUBLEVEL=65
+KEXTRAVERSION=
+KRTV=
+
+KSRC_MIRROR=
+
+
+KCFG_BASE_OS="base-os/hirsute.config-5.11.0-16-generic"
+KCFG_FEATURES_DIR="features/"
+KCFG_OVERLAY="overlay/overlay.cfg"
+
+KSRC_REPO=https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git
