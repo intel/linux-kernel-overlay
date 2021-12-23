1c48c91e331912f435c2a90890e2aecc3e1ce7e6
sys_oak on gitlab runner <sys_oak@intel.com>
2021-12-23 00:38:08 -0800

    Auto update to lts2021-rt-ubuntu v5.15.7-rt23-211221T054859Z
    Source of patches:
    84f734216 rebase update to v5.15.7-rt23
    Signed-off-by: sys_oak on gitlab runner <sys_oak@intel.com>

--- config.sh
+++ config.sh
@@ -3,9 +3,9 @@
 
 KVERSION=5
 KPATCHLEVEL=15
-KSUBLEVEL=5
+KSUBLEVEL=7
 KEXTRAVERSION=
-KRTV=-rt22
+KRTV=-rt23
 
 KSRC_MIRROR=
 
@@ -14,4 +14,4 @@ KCFG_BASE_OS="base-os/hirsute.config-5.11.0-16-generic"
 KCFG_FEATURES_DIR="features/"
 KCFG_OVERLAY="overlay/overlay.cfg"
 
-KSRC_REPO=https://kernel.googlesource.com/pub/scm/linux/kernel/git/rt/linux-rt-devel
+KSRC_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-stable-rt.git/
