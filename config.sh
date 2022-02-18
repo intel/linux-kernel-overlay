079a2870339d30ba9a08f7c50ee985fb20937f14
sys_oak <sys_oak@intel.com>
2022-02-18 02:49:54 -0800

    Auto update to sandbox-mainline-tracking-rt-v5.17-rc3-rt6-ubuntu v5.17-rc3-rt6-220217T100001Z
    Source of patches:
    9c6b9aec8 quilt: ww08.4 mainline-tracking-preempt-rt for v5.17-rc3-rt6
    Signed-off-by: sys_oak <sys_oak@intel.com>

--- config.sh
+++ config.sh
@@ -5,7 +5,7 @@ KVERSION=5
 KPATCHLEVEL=17
 KSUBLEVEL=0
 KEXTRAVERSION=-rc3
-KRTV=
+KRTV=-rt6
 
 KSRC_MIRROR=
 
@@ -14,4 +14,4 @@ KCFG_BASE_OS="base-os/hirsute.config-5.11.0-16-generic"
 KCFG_FEATURES_DIR="features/"
 KCFG_OVERLAY="overlay/overlay.cfg"
 
-KSRC_REPO=https://github.com/torvalds/linux.git
+KSRC_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-rt-devel.git
