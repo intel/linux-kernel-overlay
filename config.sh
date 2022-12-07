c94fe0e9359b2e524b7520cedb728f843b0833e4
sys_oak <sys_oak@intel.com>
2022-12-06 21:09:41 -0800

    Auto update to mainline-tracking/v6.1-rc3-ubuntu v6.1-rc3-221207T045921Z
    Source of patches:
    312d82b9d quilt: ww50.3 mainline-tracking-linux for v6.1-rc3
    Signed-off-by: sys_oak <sys_oak@intel.com>

--- config.sh
+++ config.sh
@@ -14,4 +14,4 @@ KCFG_BASE_OS="base-os/hirsute.config-5.11.0-16-generic"
 KCFG_FEATURES_DIR="features/"
 KCFG_OVERLAY="overlay/overlay.cfg"
 
-KSRC_REPO=https://github.com/torvalds/linux.git
+KSRC_REPO=https://github.com/torvalds/linux.git/
