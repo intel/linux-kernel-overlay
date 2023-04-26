ff5e9523d99fb7f338c07d02e7450c84fbc6d19e
sys_oak <sys_oak@intel.com>
2023-04-25 20:02:26 -0700

    Auto update to mainline-tracking/v6.3-ubuntu v6.3-230426T021902Z
    Source of patches:
    ddb18b493 quilt: ww17.1 mainline-tracking-linux upgrade to v6.3
    Signed-off-by: sys_oak <sys_oak@intel.com>

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 # Global configurations which are used to build kernel overlay
 
-KVERSION=5
-KPATCHLEVEL=17
+KVERSION=6
+KPATCHLEVEL=3
 KSUBLEVEL=0
-KEXTRAVERSION=-rc3
+KEXTRAVERSION=
 KRTV=
 
 KSRC_MIRROR=
@@ -14,4 +14,4 @@ KCFG_BASE_OS="base-os/hirsute.config-5.11.0-16-generic"
 KCFG_FEATURES_DIR="features/"
 KCFG_OVERLAY="overlay/overlay.cfg"
 
-KSRC_REPO=https://github.com/torvalds/linux.git
+KSRC_REPO=https://github.com/torvalds/linux.git/
