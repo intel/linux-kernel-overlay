81a2f14eefaec9fc1f535493893e270f4b3a4b97
sys_oak <sys_oak@intel.com>
2023-05-04 14:58:28 -0700

    Auto update to lts2022-rt-ubuntu v6.1.26-rt8-230504T210731Z
    Source of patches:
    1bde68fba rebase update to v6.1.26-rt8
    Signed-off-by: sys_oak <sys_oak@intel.com>

--- config.sh
+++ config.sh
@@ -3,9 +3,9 @@
 
 KVERSION=6
 KPATCHLEVEL=1
-KSUBLEVEL=12
+KSUBLEVEL=26
 KEXTRAVERSION=
-KRTV=-rt7
+KRTV=-rt8
 
 KSRC_MIRROR=
 
@@ -14,4 +14,4 @@ KCFG_BASE_OS="base-os/hirsute.config-5.11.0-16-generic"
 KCFG_FEATURES_DIR="features/"
 KCFG_OVERLAY="overlay/overlay.cfg"
 
-KSRC_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-stable-rt.git/
+KSRC_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-rt-devel.git
