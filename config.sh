f9f10d27a9e2579f043fb44050690968b56654be
sys_oak <sys_oak@intel.com>
2023-05-16 20:20:02 -0700

    Auto update to lts2022-rt-ubuntu v6.1.26-rt8-230517T004121Z
    Source of patches:
    8f7ff049c Domain update for v6.1.26-rt8
    Signed-off-by: sys_oak <sys_oak@intel.com>

--- config.sh
+++ config.sh
@@ -14,4 +14,4 @@ KCFG_BASE_OS="base-os/hirsute.config-5.11.0-16-generic"
 KCFG_FEATURES_DIR="features/"
 KCFG_OVERLAY="overlay/overlay.cfg"
 
-KSRC_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-rt-devel.git
+KSRC_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-stable-rt.git/
