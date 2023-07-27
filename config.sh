2741eb2b6ea47951c8e19d19089f023c188848ab
sys_oak <sys_oak@intel.com>
2023-07-26 19:02:48 -0700

    Auto update to mainline-tracking-rt/v6.4-rt6-ubuntu v6.4-rt6-230727T015607Z
    Source of patches:
    c74864de7 quilt: ww30.3 mainline-tracking-rt for v6.4-rt6
    Signed-off-by: sys_oak <sys_oak@intel.com>

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 # Global configurations which are used to build kernel overlay
 
-KVERSION=5
-KPATCHLEVEL=18
+KVERSION=6
+KPATCHLEVEL=4
 KSUBLEVEL=0
 KEXTRAVERSION=
-KRTV=-rt11
+KRTV=-rt6
 
 KSRC_MIRROR=
 
