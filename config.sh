e2bcaabfbeb24e0414d5d370a826c766687a3036
sys_oak <sys_oak@intel.com>
2024-04-09 23:13:14 -0700

    Auto update to mainline-tracking/v6.9-rc3-ubuntu v6.9-rc3-240410T060143Z
    Source of patches:
    6c849c70f quilt: ww15.3 mainline-tracking-linux for v6.9-rc3
    Signed-off-by: sys_oak <sys_oak@intel.com>

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 # Global configurations which are used to build kernel overlay
 
-KVERSION=5
-KPATCHLEVEL=17
+KVERSION=6
+KPATCHLEVEL=9
 KSUBLEVEL=0
 KEXTRAVERSION=-rc3
 KRTV=
@@ -14,4 +14,4 @@ KCFG_BASE_OS="base-os/hirsute.config-5.11.0-16-generic"
 KCFG_FEATURES_DIR="features/"
 KCFG_OVERLAY="overlay/overlay.cfg"
 
-KSRC_REPO=https://github.com/torvalds/linux.git
+KSRC_REPO=https://github.com/torvalds/linux.git/
