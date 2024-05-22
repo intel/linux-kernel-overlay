41603a693e6b2a010d90f739851a0b060455b13b
sys_oak <sys_oak@intel.com>
2024-05-21 23:02:54 -0700

    Auto update to mainline-tracking-rt/v6.9-rt5-ubuntu v6.9-rt5-240522T055145Z
    Source of patches:
    1f417dc37 quilt: ww21.3 mainline-tracking-rt for v6.9-rt5
    Signed-off-by: sys_oak <sys_oak@intel.com>

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 # Global configurations which are used to build kernel overlay
 
-KVERSION=5
-KPATCHLEVEL=18
+KVERSION=6
+KPATCHLEVEL=9
 KSUBLEVEL=0
 KEXTRAVERSION=
-KRTV=-rt11
+KRTV=-rt5
 
 KSRC_MIRROR=
 
