28a97fe0667ee7b65ac4c2e71894a26ddff10606
sys_oak <sys_oak@intel.com>
2024-12-17 13:17:38 -0800

    Auto update to lts2024-ubuntu v6.12.3-241217T200918Z
    Source of patches:
    d9344a8ec Kernel update 241217T200918Z
    Signed-off-by: sys_oak <sys_oak@intel.com>

--- config.sh
+++ config.sh
@@ -10,7 +10,7 @@ KRTV=
 KSRC_MIRROR=
 
 
-KCFG_BASE_OS="base-os/hirsute.config-5.11.0-16-generic"
+KCFG_BASE_OS="base-os/noble.config-6.8.0-31-generic"
 KCFG_FEATURES_DIR="features/"
 KCFG_OVERLAY="overlay/overlay.cfg"
 
