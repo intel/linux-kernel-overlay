d9799790f7cecd55fa733cfc287759835f309d28
sys_oak <sys_oak@intel.com>
2024-12-29 23:11:16 -0800

    Auto update to lts2024-ubuntu v6.12.7-241230T031102Z
    Source of patches:
    ec990dd9e rebase update to v6.12.7
    Signed-off-by: sys_oak <sys_oak@intel.com>

--- config.sh
+++ config.sh
@@ -14,4 +14,4 @@ KCFG_BASE_OS="base-os/noble.config-6.8.0-31-generic"
 KCFG_FEATURES_DIR="features/"
 KCFG_OVERLAY="overlay/overlay.cfg"
 
-KSRC_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/
+KSRC_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
