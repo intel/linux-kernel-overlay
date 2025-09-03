5844bbb2c86e64e668d0dd20f02778fc6b9ba352
sys_oak <sys_oak@intel.com>
2025-09-02 22:03:42 -0700

    Auto update to mainline-tracking/v6.17-rc3-ubuntu v6.17-rc3-250903T045120Z
    Source of patches:
    37f099275 quilt: ww36.3 mainline-tracking-linux for v6.17-rc3
    Signed-off-by: sys_oak <sys_oak@intel.com>

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 # Global configurations which are used to build kernel overlay
 
-KVERSION=
-KPATCHLEVEL=
-KSUBLEVEL=
-KEXTRAVERSION=
+KVERSION=6
+KPATCHLEVEL=17
+KSUBLEVEL=0
+KEXTRAVERSION=-rc3
 KRTV=
 
 KSRC_MIRROR=
