07e453a49fe608f34d63c29e1c0ad35c9e53e1fa
sys-oak <sys_oak@intel.com>
2026-01-07 14:02:44 +0800

    Auto update to lts2025-deb lts-preprod-v6.18.3-linux-260107T055845Z
    Source of patches:
        Quilt: 27295a017 rebase update to v6.18.3
    
    Signed-off-by: sys-oak <sys_oak@intel.com>

--- config.sh
+++ config.sh
@@ -4,15 +4,15 @@
 # shellcheck disable=SC2034
 KVERSION=6
 KPATCHLEVEL=18
-KSUBLEVEL=0
+KSUBLEVEL=3
 KEXTRAVERSION=
 KRTV=
 
 KSRC_MIRROR=
 
 
-KCFG_BASE_OS="base-os/noble.config-6.8.0-31-generic"
-KCFG_FEATURES_DIR="features/"
-KCFG_RT="rt/rt.cfg"
+KCFG_BASE_OS=base-os/noble.config-6.8.0-31-generic
+KCFG_FEATURES_DIR=features
+KCFG_RT=rt/rt.cfg
 
 KSRC_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux
