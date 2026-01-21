b23e46135aa662de9530132d4fd768e04729b6d9
sys-oak <sys_oak@intel.com>
2026-01-21 16:45:37 +0800

    Auto update to mainline-tracking/v6.19-rc3-deb mainline-preprod-v6.19-rc3-linux-260121T084140Z
    Source of patches:
        Quilt: a8060f6a3 quilt: ww04.3 mainline-tracking-linux for v6.19-rc3
    
    Signed-off-by: sys-oak <sys_oak@intel.com>

---
+++ kernel-config/conf.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#
+# Constants
+#
+# default key
+DK='default'
+# rt key
+RK='rt'
+# default variant key
+DVK='deb'
+declare -A BASE_PATH=(
+    [mainline-tracking/v6.19-rc3,$DVK]='overlay/base-os/noble.config-6.8.0-31-generic'
+    [$DK,$DVK]='overlay/base-os/noble.config-6.8.0-31-generic'
+)
+# Key rules for KCONF_PATHS:
+#   There are three fields of the keys:
+#     1 sequence number: set the handling order
+#     2 type: dir, cfg, rt
+#       dir: folder which contains the kernel config file
+#       cfg: single kernel config file
+#       rt: the kernel config file for the real-time mode
+#     3 the name of the path variable
+declare -A KCONF_PATHS=(
+    [1,dir,features_dir]='overlay/features'
+    [2,cfg,overlay]='overlay/overlay/overlay.cfg'
+)
